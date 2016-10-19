2f8088e3c6d553b570c3639e90169ffad052b026
lkl0305 <lkl0305@163.com>
2016-10-19 15:33:30 +0800

    systick实现延时函数
    systick作为us和ms级的延时函数，采用查询方式，可用在DHT11等模块上作为软件延时函数

---
+++ systick_delay/Debug/delay/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../delay/delay.c 
+
+OBJS += \
+./delay/delay.o 
+
+C_DEPS += \
+./delay/delay.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+delay/%.o: ../delay/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU C Compiler'
+	arm-none-eabi-gcc -DDEBUG -D__CODE_RED -DCORE_M0PLUS -D__MTB_BUFFER_SIZE=256 -D__USE_ROMDIVIDE -D__USE_LPCOPEN -D__LPC82X__ -D__REDLIB__ -I"D:\Board\LPC824Lite\workspace\systick_delay\inc" -I"D:\Board\LPC824Lite\workspace\systick_delay" -I"D:\Board\LPC824Lite\workspace\lpc_board_ucdrogon_lpc824lite\inc" -I"D:\Board\LPC824Lite\workspace\lpc_chip_82x\inc" -O0 -fno-common -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -mcpu=cortex-m0 -mthumb -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
