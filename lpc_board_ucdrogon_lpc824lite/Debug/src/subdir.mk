f56967b3f001b0cc29ebac86668f106c3349d45a
lkl0305 <lkl0305@163.com>
2016-10-19 12:42:10 +0800

    LPC824Lite开发板的库文件
    LPC824Lite开发板的库文件l

---
+++ lpc_board_ucdrogon_lpc824lite/Debug/src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/board.c \
+../src/board_sysinit.c 
+
+OBJS += \
+./src/board.o \
+./src/board_sysinit.o 
+
+C_DEPS += \
+./src/board.d \
+./src/board_sysinit.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU C Compiler'
+	arm-none-eabi-gcc -D__REDLIB__ -DDEBUG -D__CODE_RED -DCORE_M0PLUS -D__USE_LPCOPEN -D__LPC82X__ -I"D:\Board\LPC824Lite\workspace\lpc_board_ucdrogon_lpc824lite\inc" -I"D:\Board\LPC824Lite\workspace\lpc_chip_82x\inc" -O0 -fno-common -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -mcpu=cortex-m0 -mthumb -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
